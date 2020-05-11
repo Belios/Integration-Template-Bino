fb200dbce9ebd7b942d434aaaf9410904cd3260c
Warren Maunier <wmaunier@codeur.online>
2020-05-11 16:51:31 +0200

    Integration-Template-Bino

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Warren Maunier">
+  <title>Présentation HTML et CSS</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+  <link rel="stylesheet" href="main.css">
+
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light mynav">
+  <a class="navbar-brand" href="index.php">
+  <img src="Images/logo.png" class="d-inline-block align-top">
+</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+    <div class="topnav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PORTFOLIO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PRICING</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">TEAM</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">BLOG</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+
+  include "header.php";
+
+?>
+
+<section id="home">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/homebgwhite.jpg" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+        <p> Our Clients Are Our First Priority </p><Br>
+<div class="container w-75">
+<h1 class="display-1"> <strong>WELCOME TO BINO</strong></h1>
+    <br><br>
+    <div class="container w-25 ">
+      <div class"row">
+      <div class="d-flex justify-content-between align-items-center">
+      <p> __________ <p>
+      <img src="images/round.png">
+      <p> __________ <p>
+      </div>
+    </div>
+      </div>
+    </div>
+      <br><br>
+      <div class="container w-75">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+    </div>
+    <br>
+    <div class="button"><button type="button" class="btn btn-outline-secondary">Get Started Now</button>
+    <button type="button" class="btn btn-outline-secondary">Learn More</button>
+  </div>
+    <br><br><br><br><br><br><br><br><br><br><br>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+</section>
+
+
+<button type="button" class="btn btn-default btn-circle btn-xl justify-content-center"><i class="fa fa-arrow-down"></i> </button>
+<br><br><br><br><br><br><br><br>
+<section id="Features" class="d-flex align-items-end">
+<div class="container">
+  <div class="row align-items-start">
+    <div class="d-flex justify-content-around">
+
+      <div class="col-sm-3 mr-5">
+        <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
+        <i class="fa fa-clone mt-3"></i>
+      </div>
+            <div class="text-center">
+            <h5>SLEEK DESIGN</h5>
+              <p> ________ <p>
+          </div>
+          <div class="text-center">
+              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
+          </div>
+      </div>
+      <div class="col-sm-3 mr-5 ">
+        <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
+        <i class="fa fa-heart-o mt-3"></i>
+      </div>
+        <div class="text-center">
+          <h5>CLEAN CODE</h5>
+            <p> ________ <p>
+            </div>
+              <div class="text-center">
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
+        </div>
+    </div>
+      <div class="col-sm-3 mr-5">
+        <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
+        <i class="fa fa-lightbulb-o mt-3"></i>
+        </div>
+        <div class="text-center">
+          <h5>CREATIVE IDEAS</h5>
+            <p> ________ <p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+        </div>
+      </div>
+      <div class="col-sm-3 mr-5">
+        <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
+        <i class="fa fa-comments-o mt-3"></i>
+        </div>
+        <div class="text-center">
+          <h5>FREE SUPPORT</h5>
+            <p> ________ <p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+        </div>
+    </div>
+  </div>
+</div>
+</section>
+
+
+
+
+
+</body>
+
+
+<?php
+include "footer.php";
