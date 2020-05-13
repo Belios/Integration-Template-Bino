d17e9ab5de83a0dd6e8f3d3180f3573ce74ae10a
Belios <64775563+Belios@users.noreply.github.com>
2020-05-13 14:57:41 +0200

    template bino

--- index.php
+++ index.php
@@ -5,85 +5,81 @@
 ?>
 
 <section id="home"> <!-- Page d'accueil -->
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block mw" src="images/homebgwhite.jpg" alt="First slide">
-        <div class="carousel-caption d-none d-md-block">
-        <p> Our Clients Are Our First Priority </p><Br>
-<div class="container w-75">
-<h1 class="display-1"> <strong>WELCOME TO BINO</strong></h1>
-    <br><br>
-    <div class="container w-25 ">
-      <div class"row">
-      <div class="d-flex justify-content-between align-items-center">
-      <p class="line"> __________ </p>
-      <img src="images/round.png">
-      <p class="line"> __________ </p>
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block mw" src="images/homebgwhite.jpg" alt="First slide">
+          <div class="carousel-caption d-none d-md-block">
+            <p> Our Clients Are Our First Priority </p><Br>
+              <div class="container w-75">
+                <h1 class="display-1"> <strong>WELCOME TO BINO</strong></h1>
+                <br><br>
+                <div class="container w-25 ">
+                  <div class"row">
+                    <div class="d-flex justify-content-between align-items-center">
+                      <p class="line"> __________ </p>
+                      <img src="images/round.png">
+                      <p class="line"> __________ </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <br><br>
+              <div class="container w-75">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+              </div>
+              <br>
+              <div class="button"><button type="button" class="btn btn-outline-secondary">Get Started Now</button>
+                <button type="button" class="btn btn-outline-secondary">Learn More</button>
+              </div>
+              <button type="button" class="btn btn-default btn-circle btn-xl justify-content-center"><a href="#Features"><i   class="fa fa-arrow-down"></i></a></button>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
       </div>
-    </div>
-      </div>
-    </div>
-      <br><br>
-      <div class="container w-75">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
-    </div>
-    <br>
-    <div class="button"><button type="button" class="btn btn-outline-secondary">Get Started Now</button>
-    <button type="button" class="btn btn-outline-secondary">Learn More</button>
-  </div>
-      <button type="button" class="btn btn-default btn-circle btn-xl justify-content-center"><a href="#Features"><i class="fa fa-arrow-down"></i></a></button>
-
-    </div>
-  </div>
-
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</div>
-</section> <!-- Fin page d'accueil -->
-
+    </section> <!-- Fin page d'accueil -->
 
 <br><br><br><br><br><br><br><br>
 
 <section id="Features" class="d-flex align-items-end"> <!-- Page features -->
-<div class="container">
-  <div class="row align-items-start">
-    <div class="d-flex justify-content-around">
+  <div class="container">
+    <div class="row align-items-start">
+      <div class="d-flex justify-content-around">
 
-      <div class="col-sm-3 mr-5">
-        <span class="hovering">
+        <div class="col-sm-3 mr-5">
+          <span class="hovering">
           <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
             <i class="fa fa-clone mt-3"></i>
           </div>
             <div class="text-center">
-            <h5>SLEEK<span class="underline"> DE</span>SIGN</h5>
-          </span>
-          </div>
+              <h5>SLEEK<span class="underline"> DE</span>SIGN</h5>
+            </span>
+            </div>
           <div class="text-center">
             <br>
-              <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
+            <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
           </div>
-      </div>
+        </div>
       <div class="col-sm-3 mr-5 ">
         <span class="hovering">
-        <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
-        <i class="fa fa-heart-o mt-3"></i>
-      </div>
+          <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
+            <i class="fa fa-heart-o mt-3"></i>
+          </div>
         <div class="text-center">
-          <h5>CLEA<span class="underline">N C</span>ODE</h5>
-        </span>
+          <h5>CLEA<span class="underline">N C</span>ODE</h5></span>
             <br>
-            </div>
-              <div class="text-center">
-            <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
         </div>
+      <div class="text-center">
+        <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
+      </div>
     </div>
       <div class="col-sm-3 mr-5">
         <span class="hovering">
@@ -102,10 +98,9 @@
         <span class="hovering">
           <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
             <i class="fa fa-comments-o mt-3"></i>
-        </div>
+          </div>
         <div class="text-center">
-          <h5>FREE <span class="underline">SUP</span>PORT</h5>
-          </span>
+          <h5>FREE <span class="underline">SUP</span>PORT</h5> </span>
             <br>
             <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
         </div>
@@ -134,7 +129,7 @@
           The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' <p>
           </div>
 
-       <button type="button" class="btn btn-danger mt-5">BROWSE OUR WORK</button>
+          <button type="button" class="btn btn-danger mt-5">BROWSE OUR WORK</button>
         </div>
       </div>
     </div>
@@ -145,18 +140,18 @@
 
 
 
-<section id="Services">
+<section id="Services"> <!-- Page Services -->
   <div class="container-fluid">
     <div class="row">
       <div class="col-sm-6 half-left">
         <div class="container-1 border-right border-5 border-dark">
         <h3 id="h3-1"> OUR SERVICES </h3>
-      </div>
-          <div class="row-a d-flex justify-content-end">
-            <div class="border-1 border col-sm-9 mt-5 d-flex justify-content-end">
-              <span class="color">
-                <h5 class="tlepos">WEB DESIGN</h5>
-              <p class="txtpos">Lorem Ipsum is simply dummy text of the printing and tyepesetting industry. Lorem Ip sum has been the industry's standard dummy text ever.</p></span>
+        </div>
+        <div class="row-a d-flex justify-content-end">
+          <div class="border-1 border col-sm-9 mt-5">
+            <div class="container color">
+              <h5 class="tlepos d-flex justify-content-end">WEB DESIGN</h5>
+              <p class="txtpos">Lorem Ipsum is simply dummy text of the printing and tyepesetting industry. Lorem Ip sum has been the industry's standard dummy text ever.</p></div>
           </div>
         </div>
 
