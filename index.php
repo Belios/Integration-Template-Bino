ba2f6af720e54d67bcd48a8c715188b50abc315f
Belios <64775563+Belios@users.noreply.github.com>
2020-05-13 14:27:38 +0200

    template Binos

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 
 ?>
 
-<section id="home">
+<section id="home"> <!-- Page d'accueil -->
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="images/homebgwhite.jpg" alt="First slide">
+      <img class="d-block mw" src="images/homebgwhite.jpg" alt="First slide">
         <div class="carousel-caption d-none d-md-block">
         <p> Our Clients Are Our First Priority </p><Br>
 <div class="container w-75">
@@ -17,9 +17,9 @@
     <div class="container w-25 ">
       <div class"row">
       <div class="d-flex justify-content-between align-items-center">
-      <p> __________ <p>
+      <p class="line"> __________ </p>
       <img src="images/round.png">
-      <p> __________ <p>
+      <p class="line"> __________ </p>
       </div>
     </div>
       </div>
@@ -32,9 +32,11 @@
     <div class="button"><button type="button" class="btn btn-outline-secondary">Get Started Now</button>
     <button type="button" class="btn btn-outline-secondary">Learn More</button>
   </div>
-    <br><br><br><br><br><br><br><br><br><br><br>
+      <button type="button" class="btn btn-default btn-circle btn-xl justify-content-center"><a href="#Features"><i class="fa fa-arrow-down"></i></a></button>
+
     </div>
   </div>
+
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -45,68 +47,130 @@
   </a>
 </div>
 </div>
-</section>
+</section> <!-- Fin page d'accueil -->
 
 
-<button type="button" class="btn btn-default btn-circle btn-xl justify-content-center"><i class="fa fa-arrow-down"></i> </button>
 <br><br><br><br><br><br><br><br>
-<section id="Features" class="d-flex align-items-end">
+
+<section id="Features" class="d-flex align-items-end"> <!-- Page features -->
 <div class="container">
   <div class="row align-items-start">
     <div class="d-flex justify-content-around">
 
       <div class="col-sm-3 mr-5">
-        <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
-        <i class="fa fa-clone mt-3"></i>
-      </div>
+        <span class="hovering">
+          <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
+            <i class="fa fa-clone mt-3"></i>
+          </div>
             <div class="text-center">
-            <h5>SLEEK DESIGN</h5>
-              <p> ________ <p>
+            <h5>SLEEK<span class="underline"> DE</span>SIGN</h5>
+          </span>
           </div>
           <div class="text-center">
-              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
+            <br>
+              <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
           </div>
       </div>
       <div class="col-sm-3 mr-5 ">
+        <span class="hovering">
         <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
         <i class="fa fa-heart-o mt-3"></i>
       </div>
         <div class="text-center">
-          <h5>CLEAN CODE</h5>
-            <p> ________ <p>
+          <h5>CLEA<span class="underline">N C</span>ODE</h5>
+        </span>
+            <br>
             </div>
               <div class="text-center">
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
+            <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
         </div>
     </div>
       <div class="col-sm-3 mr-5">
-        <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
-        <i class="fa fa-lightbulb-o mt-3"></i>
+        <span class="hovering">
+          <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
+            <i class="fa fa-lightbulb-o mt-3"></i>
+          </div>
+          <div class="text-center">
+            <h5>CREATI<span class="underline">VE </span>IDEAS</h5>
+          </span>
+            <br>
+            <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+          </div>
+        </div>
+
+      <div class="col-sm-3 mr-5">
+        <span class="hovering">
+          <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
+            <i class="fa fa-comments-o mt-3"></i>
         </div>
         <div class="text-center">
-          <h5>CREATIVE IDEAS</h5>
-            <p> ________ <p>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+          <h5>FREE <span class="underline">SUP</span>PORT</h5>
+          </span>
+            <br>
+            <p id="serif"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
         </div>
       </div>
-      <div class="col-sm-3 mr-5">
-        <div class="border border-dark mb-4 w-25 h-25 mx-auto d-flex justify-content-center">
-        <i class="fa fa-comments-o mt-3"></i>
-        </div>
-        <div class="text-center">
-          <h5>FREE SUPPORT</h5>
-            <p> ________ <p>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
-        </div>
     </div>
   </div>
 </div>
+</section> <!-- Fin page features -->
+
+<hr>
+<br><br><br><br><br>
+
+<section id="history" > <!-- Page history -->
+  <div class="container">
+    <div class="row">
+      <div class="d-flex bd-highlight">
+        <div class="p-2 flex-fill bd-highlight mr-3">
+        <img src="images/stab1.png">
+        </div>
+
+
+        <div class="p-2 flex-fill bd-highlight ml-4">
+          <div class="container border-left border-5 border-dark ml-4 mb-4"> <h3> OUR HISTORY </h3> </div>
+          <div class="container ml-2 mt-5">
+          <p id="sans-serif">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. <br><br>
+          The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' <p>
+          </div>
+
+       <button type="button" class="btn btn-danger mt-5">BROWSE OUR WORK</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section> <!-- Fin History -->
+
+<br><br><br><br><br>
+
+
+
+<section id="Services">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 half-left">
+        <div class="container-1 border-right border-5 border-dark">
+        <h3 id="h3-1"> OUR SERVICES </h3>
+      </div>
+          <div class="row-a d-flex justify-content-end">
+            <div class="border-1 border col-sm-9 mt-5 d-flex justify-content-end">
+              <span class="color">
+                <h5 class="tlepos">WEB DESIGN</h5>
+              <p class="txtpos">Lorem Ipsum is simply dummy text of the printing and tyepesetting industry. Lorem Ip sum has been the industry's standard dummy text ever.</p></span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-sm-4 half-right">
+        <img src="images/servicerightimg.jpg">
+
+
+      </div>
+    </div>
+  </div>
+
 </section>
-
-
-
-
-
 </body>
 
 
